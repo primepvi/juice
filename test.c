bc235950ba26c3a4a09b288abee10379fe32295d
primepvi <pedrobernardesv@gmail.com>
2025-05-10 23:08:20 -0300

    docs: creating more complex example.

--- test.c
+++ test.c
@@ -1,11 +1,25 @@
 #define JUICE_IMPLEMENTATION
+#define JUICE_CANVAS_WIDTH 41
+#define JUICE_CANVAS_HEIGHT 20
 #include "juice.h"
 
 int main() {
   juice_begin_frame();
-  juice_put_rect(JUICE_VEC2(7, 4), JUICE_SIZE(16, 8),
-                 JUICE_PX(1, 0, JUICE_DOT));
-  juice_end_frame();
 
+  // yellow face
+  juice_put_rect(JUICE_VEC2(10, 5), JUICE_SIZE(20, 10),
+                 JUICE_PX(226, 226, JUICE_SOLID));
+
+  // black eyes
+  juice_put_rect(JUICE_VEC2(14, 8), JUICE_SIZE(2, 2),
+                 JUICE_PX(0, 0, JUICE_SOLID));
+  juice_put_rect(JUICE_VEC2(24, 8), JUICE_SIZE(2, 2),
+                 JUICE_PX(0, 0, JUICE_SOLID));
+
+  // red mouth
+  juice_put_rect(JUICE_VEC2(16, 12), JUICE_SIZE(8, 1),
+                 JUICE_PX(160, 0, JUICE_SOLID));
+
+  juice_end_frame();
   return 0;
 }
